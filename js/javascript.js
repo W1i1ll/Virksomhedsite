ea5952f0223eb9b5b1fb76363185a3a07d985fa6
Will <wisa0001@stud.kea.dk>
2023-05-30 13:58:00 +0200

    modified:   js/javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -10,5 +10,6 @@ Denne kode vælger alle <article> og <figure> elementer i dokumentet og anvender
 
 Funktionen addIndex tager to parametre: item repræsenterer hvert enkelt valgt element, og index repræsenterer indekset eller positionen for elementet i udvælgelsen.
 
-Inde i addIndex funktionen sætter linjen item.style.setProperty('--i', index + 1) en brugerdefineret CSS-variabel kaldet --i til værdien af index + 1. Dette kan bruges til at tilpasse stylingen eller layoutet af elementerne baseret på deres position i udvælgelsen.
+Inde i addIndex funktionen sætter linjen item.style.setProperty('--i', index + 1) en brugerdefineret CSS-variabel kaldet --i til værdien af index + 1. 
+Dette kan bruges til at tilpasse stylingen eller layoutet af elementerne baseret på deres position i udvælgelsen.
 */
